7a506efae0e17fd5677eb3db908d349a44703a78
Pushkar Agrawal <pushkaragrawal1702@gmail.com>
2025-07-04 19:09:33 +0530

    Updated configure.example.js

--- config.example.js
+++ config.example.js
@@ -1,1 +1,11 @@
-const apiKey = "YOUR_TMDB_API_KEY_HERE";
+const apiKey = "YOUR_TMDB_API_KEY";
+const firebaseConfig = {
+    apiKey: "YOUR_FIREBASE_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "...",
+    appId: "..."
+};
+
+firebase.initializeApp(firebaseConfig);
